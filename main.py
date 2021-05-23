edfba622fd605cb1e84b3e54d55f7c4bbf25e06d
Mike Arnhold <mike.arnhold@tu-dresden.de>
2021-05-23 22:14:25 +0200

    feat(main): index with endpoint decorator

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ from typing import Dict
 
 from flask import Flask
 
+from decorators import endpoint, template
+
 app = Flask("TDD")
 
 
-@app.route("/")
+@endpoint(app.route("/"), template("index.html"))
 def index() -> Dict[str, str]:
     """Index view"""
     return dict(greeting="Hello, World!")
