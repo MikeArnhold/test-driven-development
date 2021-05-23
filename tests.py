fd8c20bdc542a4893691fcba95c792a7f81a627c
Mike Arnhold <mike.arnhold@tu-dresden.de>
2021-05-23 18:37:31 +0200

    feat(decorator): add view_format with 1st test

--- tests.py
+++ tests.py
@@ -1,8 +1,10 @@
 """Test implementations"""
-
 from unittest import TestCase
 
-from main import index
+from flask import render_template_string
+
+from decorators import view_format
+from main import app, index
 
 
 class TestIndex(TestCase):
@@ -11,3 +13,17 @@ class TestIndex(TestCase):
     def test_index(self):
         """Index returns greetind as data dict"""
         self.assertEqual({"greeting": "Hello, World!"}, index())
+
+
+class ViewFormatTests(TestCase):
+    """view_format tests"""
+
+    def test_render_div_foo_42(self) -> None:
+        """render foo=42 within div"""
+
+        @view_format(render_template_string, r"<div>{{ foo }}</div>")
+        def view():
+            return dict(foo=42)
+
+        with app.app_context():
+            self.assertEqual("<div>42</div>", view())
