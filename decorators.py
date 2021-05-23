4cfa64c434f83ee5d7e0f9b05b073fcd026fc21b
Mike Arnhold <mike.arnhold@tu-dresden.de>
2021-05-23 18:37:31 +0200

    feat(decorator): add template_render with 1st test

---
+++ decorators.py
@@ -0,0 +1,19 @@
+"""Decorator implementations"""
+from typing import Any, Callable, Dict, Text
+
+TRender = Callable[[Text, Any], Text]
+
+TView = Callable[..., Dict[str, Any]]
+TViewDecorator = Callable[[TView], TView]
+
+
+def template_render(render_fn: TRender, render_str: Text) -> TViewDecorator:
+    """Decorator factory to render view data via template"""
+
+    def decorator(view: TView) -> TView:
+        def wrapper():
+            return ""
+
+        return wrapper
+
+    return decorator
